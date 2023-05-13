4e0c67a2818fa40ab1fcfa9517b0a1bf78cba1d4
sora <sorazora1.4@gmail.com>
2023-05-12 20:17:17 -0700

    prefetches profile page with ssg

---
+++ src/pages/profiles/[id].tsx
@@ -0,0 +1,61 @@
+import type {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetServerSidePropsType,
+  NextPage,
+} from "next";
+import Head from "next/head";
+import { ssgHelper } from "~/server/api/ssgHelper";
+import { api } from "~/utils/api";
+import ErrorPage from "next/error";
+
+const ProfilePage: NextPage<
+  InferGetServerSidePropsType<typeof getStaticProps>
+> = ({ id }) => {
+  const { data: profile } = api.profile.getById.useQuery({ id });
+
+  if (profile == null || profile.name == null) {
+    return <ErrorPage statusCode={404} />;
+  }
+  return (
+    <>
+      <Head>
+        <title>{`Tweet3 App ${profile.name}`}</title>
+      </Head>
+      {profile.name}
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export async function getStaticProps(
+  context: GetStaticPropsContext<{ id: string }>
+) {
+  const id = context.params?.id;
+
+  if (id === undefined) {
+    return {
+      redirect: {
+        destination: "/",
+      },
+    };
+  }
+
+  const ssg = ssgHelper();
+  await ssg.profile.getById.prefetch({ id });
+
+  return {
+    props: {
+      id,
+      trpcState: ssg.dehydrate(),
+    },
+  };
+}
+
+export default ProfilePage;
