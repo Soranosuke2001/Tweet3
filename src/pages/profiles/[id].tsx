a0940bbf025d1462292efc427a7934d7a29c2c5b
sora <sorazora1.4@gmail.com>
2023-05-12 20:25:54 -0700

    displays the user stats on porfile page

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -8,6 +8,10 @@ import Head from "next/head";
 import { ssgHelper } from "~/server/api/ssgHelper";
 import { api } from "~/utils/api";
 import ErrorPage from "next/error";
+import Link from "next/link";
+import IconHoverEffect from "~/components/IconHoverEffect";
+import { VscArrowLeft } from "react-icons/vsc";
+import ProfileImage from "~/components/ProfileImage";
 
 const ProfilePage: NextPage<
   InferGetServerSidePropsType<typeof getStaticProps>
@@ -22,10 +26,33 @@ const ProfilePage: NextPage<
       <Head>
         <title>{`Tweet3 App ${profile.name}`}</title>
       </Head>
-      {profile.name}
+      <header className="sticky top-0 z-10 flex items-center border-b bg-white px-4 py-2">
+        <Link href=".." className="mr-2">
+          <IconHoverEffect>
+            <VscArrowLeft className="h-6 w-6" />
+          </IconHoverEffect>
+        </Link>
+        <ProfileImage src={profile.image} className="flex-shrink-0" />
+        <div className="ml-2 flex-grow">
+          <h1 className="text-lg font-bold">{profile.name}</h1>
+          <div className="text-gray-500">
+            {profile.tweetsCount}{" "}
+            {getPlural(profile.tweetsCount, "Tweet", "Tweets")} -{" "}
+            {profile.followersCount}{" "}
+            {getPlural(profile.tweetsCount, "Follower", "Followers")} -{" "}
+            {profile.followsCount} Following
+          </div>
+          {/* <FollowButton isFollowing={profile.isFollowing} userId={id} onClick={() => null} /> */}
+        </div>
+      </header>
     </>
   );
 };
+
+const pluralRules = new Intl.PluralRules();
+function getPlural(number: number, singular: string, plural: string) {
+  return pluralRules.select(number) === "one" ? singular : plural;
+}
 
 export const getStaticPaths: GetStaticPaths = () => {
   return {
